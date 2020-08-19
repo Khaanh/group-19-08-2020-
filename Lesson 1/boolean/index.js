5a6d9e48493edf4d3c18e874ce1422bf7081aa34
Vadim Kuznetsov <v.kuznetsov@intetics.com>
2020-08-20 00:44:39 +0300

    first lesson

---
+++ Lesson 1/boolean/index.js
@@ -0,0 +1,19 @@
+var t = null;
+var f = 0;
+
+// Таблица истинности логического И (&&):
+//             true  && true = true
+//             false && true = false
+//             true  && false = false
+//             false && false = false
+
+console.log('and', t && f);
+
+// Таблица истинности логического ИЛИ (||):
+//             true  || true = true
+//             false || true = true
+//             true  || false = true
+//             false || false = false
+
+
+console.log('or',t || f);
---
+++ Lesson 1/string/index.js
@@ -0,0 +1,3 @@
+var str = '10px';
+console.log(+str);
+console.log(+str + 10);
